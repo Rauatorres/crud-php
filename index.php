08a71e8d195223766571135e9e12318f93cb84b1
raua <rauatorres@gmail.com>
2021-10-19 16:17:15 -0300

    criando formulário

--- index.php
+++ index.php
@@ -5,9 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Página Principal</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
 </head>
 <body>
     <a href="#cadastrar">Cadastrar Pessoa</a>
+    <form action="#" method="post">
+        <label for="">Nome</label>
+        <input type="text" name="" id="">
+        <label for="">Sexo</label>
+        <label for="">M</label>
+        <input type="radio" name="" id="masc">
+        <label for="">F</label>
+        <input type="radio" name="" id="fem">
+        <label for="">Data de Nascimento</label>
+        <input type="date" name="" id="">
+        <label for="">Telefone</label>
+        <input type="tel" name="" id="">
+        <label for="">E-mail</label>
+        <input type="email" name="" id="">
+        <input type="submit" value="Enviar">
+        </form>
     <table>
         <tr>
             <td>Id</td>
@@ -18,8 +36,5 @@
             <td>Email</td>
         </tr>
     </table>
-    <?php
-        echo "Olá Mundo!";
-    ?>
 </body>
 </html>
