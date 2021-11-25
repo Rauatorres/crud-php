0698600672d763d64e0d77427e564ee10b564ef5
Rauatorres <rauatorres@gmail.com>
2021-11-25 19:12:56 -0200

    tamanho e posicionamento no formulário principal

--- index.php
+++ index.php
@@ -10,22 +10,29 @@
 </head>
 <body>
     <a href="#cadastrar">Cadastrar Pessoa</a>
-    <form action="#" method="post">
-        <label for="">Nome</label>
-        <input type="text" name="" id="">
+    <form action="#" method="post" id="formPrincipal">
+        <label for="nome">Nome</label>
+        <input type="text" name="" id="nome">
         <label for="">Sexo</label>
-        <label for="">M</label>
-        <input type="radio" name="" id="masc">
-        <label for="">F</label>
-        <input type="radio" name="" id="fem">
-        <label for="">Data de Nascimento</label>
-        <input type="date" name="" id="">
-        <label for="">Telefone</label>
-        <input type="tel" name="" id="">
-        <label for="">E-mail</label>
-        <input type="email" name="" id="">
+        <div id="areaSexo">
+            <label for="masc">M</label>
+            <input type="radio" name="sexo" id="masc">
+            <label for="fem">F</label>
+            <input type="radio" name="sexo" id="fem">
+        </div>
+        <label for="dataNasc">Data de Nascimento</label>
+        <input type="date" name="" id="dataNasc">
+        <label for="tel">Telefone</label>
+        <input type="tel" name="" id="tel">
+        <label for="email">E-mail</label>
+        <input type="email" name="" id="email">
         <input type="submit" value="Enviar">
-        </form>
+    </form>
+    <form id="formExcluir">
+        <p>Deseja mesmo excluir pessoa?</p>
+        <input type="button" name="" value="Cancelar">
+        <input type="button" name="" value="Ok">
+    </form>
     <table>
         <tr>
             <td>Id</td>
@@ -34,6 +41,15 @@
             <td>Data de Nascimento</td>
             <td>Telefone</td>
             <td>Email</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>Teste</td>
+            <td>M</td>
+            <td>1990-06-18</td>
+            <td>124187362</td>
+            <td>teste@teste.com</td>
+            <td><a href="#">Editar</a><a href="#">Excluir</a></td>
         </tr>
     </table>
 </body>
